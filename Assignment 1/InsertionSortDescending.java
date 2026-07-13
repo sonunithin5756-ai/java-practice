ce7fbe6bc41035db77c2d5ad3437ee12481d1cad
sonunithin <sonunithin5756@gmail.com>
2026-07-13 10:01:54 +0530

    Assignment

---
+++ Assignment 1/InsertionSortDescending.java
@@ -0,0 +1,23 @@
+public class SelectionSortDescending {
+    public static void main(String[] args) {
+        int arr[] = {64, 25, 12, 22, 11};
+        int temp;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int max = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > arr[max])
+                    max = j;
+            }
+
+            temp = arr[max];
+            arr[max] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println("Descending Order:");
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
